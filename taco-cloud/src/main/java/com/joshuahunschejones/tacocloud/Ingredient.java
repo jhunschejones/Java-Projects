624a4c29c9bfe15b6db659c8418b9619a1c82a02
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-09-30 12:39:59 -0700

    Add progress to taco-cloud

---
+++ taco-cloud/src/main/java/com/joshuahunschejones/tacocloud/Ingredient.java
@@ -0,0 +1,21 @@
+package com.joshuahunschejones.tacocloud;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public Ingredient(String id, String name, Type type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
